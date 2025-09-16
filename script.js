463cb628284a0cc8ed99a8b872d0111788bae2de
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2025-09-16 16:10:17 +0500

    refactor: reorganize static asset storage directories for media vaults

--- script.js
+++ script.js
@@ -244,3 +244,4 @@
 // Engineering 2025 checkpoint block trace: Date(2025-09-16 15:04:10) Index(14)
 // Engineering 2025 checkpoint block trace: Date(2025-09-16 15:21:36) Index(15)
 // Engineering 2025 checkpoint block trace: Date(2025-09-16 16:17:42) Index(16)
+// Engineering 2025 checkpoint block trace: Date(2025-09-16 16:10:17) Index(17)
