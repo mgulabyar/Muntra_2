c60f875a434ae83a6296b9a498e8ef41cc9b1413
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2025-09-18 11:55:31 +0500

    fix: repair asymmetric authentication processing failures inside callback

--- script.js
+++ script.js
@@ -295,3 +295,4 @@
 // Engineering 2025 checkpoint block trace: Date(2025-09-18 10:11:53) Index(5)
 // Engineering 2025 checkpoint block trace: Date(2025-09-18 11:24:54) Index(6)
 // Engineering 2025 checkpoint block trace: Date(2025-09-18 11:22:48) Index(7)
+// Engineering 2025 checkpoint block trace: Date(2025-09-18 11:55:31) Index(8)
