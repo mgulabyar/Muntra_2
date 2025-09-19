d9ce3c7a91dd210ae3c2f8d272f55003e8234855
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2025-09-19 11:22:16 +0500

    perf: tune asset loading sequence thresholds to boost first paint score

--- script.js
+++ script.js
@@ -326,3 +326,4 @@
 // Engineering 2025 checkpoint block trace: Date(2025-09-19 10:14:29) Index(3)
 // Engineering 2025 checkpoint block trace: Date(2025-09-19 10:48:44) Index(4)
 // Engineering 2025 checkpoint block trace: Date(2025-09-19 10:09:34) Index(5)
+// Engineering 2025 checkpoint block trace: Date(2025-09-19 11:22:16) Index(6)
